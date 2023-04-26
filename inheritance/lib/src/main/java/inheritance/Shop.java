afde42a78e831a825270f7503f411b8c7a168386
Alex Carr <mralex.carr@yahoo.com>
2023-04-25 18:03:53 -0700

    Shop and Theater base done

--- inheritance/lib/src/main/java/inheritance/Shop.java
+++ inheritance/lib/src/main/java/inheritance/Shop.java
@@ -1,4 +1,36 @@
 package inheritance;
 
+import java.util.ArrayList;
+
 public class Shop {
+    private String name;
+    private String description;
+    private String priceCategory;
+    private ArrayList<Review> reviews; // List of review objects per store
+
+    //Constructor Restaurant
+    public Shop(String name, String description, String priceCategory) {
+        this.name = name;
+        this.description = description;
+        this.priceCategory = priceCategory;
+        this.reviews = new ArrayList<>(); // Init list
+    }
+    //review list getter
+    public void addReview(Review review) {
+        reviews.add(review);
+    }
+
+
+    // Getter method for name
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "name='" + name + '\'' +
+                ", description=" + description +
+                ", priceCategory='" + priceCategory + '\'' +
+                '}';
+    }
+
+
 }
+
--- inheritance/lib/src/main/java/inheritance/Theater.java
+++ inheritance/lib/src/main/java/inheritance/Theater.java
@@ -1,4 +1,33 @@
 package inheritance;
 
-public class Theater {
+import java.util.ArrayList;
+
+public class Shop {
+    private String name;
+    private String movies;
+    private ArrayList<Review> reviews; // List of review objects per store
+
+    //Constructor Restaurant
+    public Shop(String name, String description, String priceCategory) {
+        this.name = name;
+        this.movies = movies;
+        this.reviews = new ArrayList<>(); // Init list
+    }
+    //review list getter
+    public void addReview(Review review) {
+        reviews.add(review);
+    }
+
+
+    // Getter method for name
+    @Override
+    public String toString() {
+        return "Theater{" +
+                "name='" + name + '\'' +
+                ", movies=" + movies +
+                '}';
+    }
+
 }
+
+
